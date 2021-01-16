8b5f141a2c36713070033c3043efb5b8e28a5584
Trevor Bonomi <trevorbonomi@gmail.com>
2021-01-16 17:31:55 +0100

    fix

--- page-portfolio.php
+++ page-portfolio.php
@@ -53,7 +53,7 @@
         </div>
         <div>
             <?php if ($theType == 'video') { ?>
-            <textarea class="theImageURL" height="<?php echo $defaultHeight;?>" width="<?php echo $defaultWidth;?>">
+            <textarea class="theImageURL">
           <div style="background:#000 url('<?php bloginfo("template_url"); ?>/images/loadingBlack.gif') center center no-repeat;">
             <video width="100%" height="100%" poster="<?php echo $theThumb; ?>" controls autoplay>
               <source src="<?php echo $theItem; ?>" type="video/mp4">
