17f77328a28f74d8418f40b1de52fac2a8e60969
Trevor Bonomi <trevorbonomi@gmail.com>
2021-01-17 15:00:58 +0100

    arf

--- sidebar.php
+++ sidebar.php
@@ -5,33 +5,33 @@
     <?php if (is_page($post_object->ID)) : ?>
       <div class="widget">
         <div class="widgetPhoto">
-        <?php if(get_field('the_video')): ?>
-        <?php if(get_field('the_image')): ?>
-          <?php $theThumb = wp_get_attachment_thumb_url(get_field('the_image'), 'thumbnail'); ?>
-        <?php else : ?>
-          <?php $theThumb = "http://www.google.com"; ?>
-        <?php endif;?>
-        <video width="100%" height="100%" poster="<?php echo $theThumb; ?>" controls preload="none">
-          <source src="<?php the_field('the_video'); ?>" type="video/mp4">
-        </video>
-        <?php elseif (get_field('the_image')): ?>
-          <?php $theLarge = wp_get_attachment_image_src( get_field('the_image'), 'medium' ); ?>
-          <?php $theThumb = wp_get_attachment_thumb_url(get_field('the_image')); ?>
-        <div class="theImageURL" style="height:<?php echo $theLarge[2]."px"; ?>; width:<?php echo $theLarge[1]."px"; ?>;">
-          <?php echo $theLarge[0] ?>
-        </div>
-        <img src="<?php echo $theThumb; ?>" width='273' height='202' style="margin-top:8px;" />
-        <?php endif; ?>
+          <?php if(get_field('the_caption')): ?>
+            <div class="widgetCaption">
+              <p><?php echo get_field('the_caption'); ?></p>
+            </div>
+          <?php endif; ?>
+          <?php if(get_field('the_video')): ?>
+          <?php if(get_field('the_image')): ?>
+            <?php $theThumb = wp_get_attachment_thumb_url(get_field('the_image'), 'thumbnail'); ?>
+          <?php else : ?>
+            <?php $theThumb = "http://www.google.com"; ?>
+          <?php endif;?>
+          <video width="100%" height="100%" poster="<?php echo $theThumb; ?>" controls preload="none">
+            <source src="<?php the_field('the_video'); ?>" type="video/mp4">
+          </video>
+          <?php elseif (get_field('the_image')): ?>
+            <?php $theLarge = wp_get_attachment_image_src( get_field('the_image'), 'medium' ); ?>
+            <?php $theThumb = wp_get_attachment_thumb_url(get_field('the_image')); ?>
+          <div class="theImageURL" style="height:<?php echo $theLarge[2]."px"; ?>; width:<?php echo $theLarge[1]."px"; ?>;">
+            <?php echo $theLarge[0] ?>
+          </div>
+          <img src="<?php echo $theThumb; ?>" width='273' height='202' style="margin-top:8px;" />
+          <?php endif; ?>
         </div>
         <img alt="Frame" class="frame fn" src="<?php bloginfo('template_url'); ?>/images/gildedFrameN.png" width="319" height="40" />
         <img alt="Frame" class="frame fw" src="<?php bloginfo('template_url'); ?>/images/gildedFrameW.png" width="34" height="185" />
         <img alt="Frame" class="frame fe" src="<?php bloginfo('template_url'); ?>/images/gildedFrameE.png" width="33" height="185" />
         <img alt="Frame" class="frame fs" src="<?php bloginfo('template_url'); ?>/images/gildedFrameS.png" width="319" height="38" />
-        <?php if(get_field('the_caption')): ?>
-          <div class="widgetCaption">
-            <p><?php echo get_field('the_caption'); ?></p>
-          </div>
-        <?php endif; ?>
       </div> <!--/widget-->
     <?php endif; //if (is_page($post_object->post_name)):?>
   <?php endwhile; ?>
